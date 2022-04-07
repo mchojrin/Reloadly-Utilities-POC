074f028500ecbb66ccc5237432295a88d5a8d1d9
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-04-07 17:22:13 +0200

    Adding docker and composer configuration

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit2f71edb51976a2721407a27ce6f81152
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit2f71edb51976a2721407a27ce6f81152', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit2f71edb51976a2721407a27ce6f81152', 'loadClassLoader'));
@@ -50,6 +52,29 @@ class ComposerAutoloaderInit2f71edb51976a2721407a27ce6f81152
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit2f71edb51976a2721407a27ce6f81152::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire2f71edb51976a2721407a27ce6f81152($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire2f71edb51976a2721407a27ce6f81152($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
